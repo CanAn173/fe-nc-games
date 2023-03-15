6242a24fbe9d88450cc07588466d5b25b6f30f22
canan173 <cacan2003@gmail.com>
2023-03-15 13:58:52 +0000

    added loading, still working on utils

--- northcoders/frontend/fe-nc-games/src/component/ReviewList.jsx
+++ northcoders/frontend/fe-nc-games/src/component/ReviewList.jsx
@@ -1,17 +1,22 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { getReviews } from '../utils/api'
+import {ReviewCard} from './ReviewCard'
 
 export const ReviewList = () => {
+
     
     const reviewsButton = axios.create({
         baseURL : 'https://my-nc-game-project.onrender.com/api'
     })
 
     const [reviews, setReviews] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
       reviewsButton.get('/reviews')
       .then((result) => {
+        setLoading(false);
         console.log(result)
         setReviews(result.data.review)
       })
@@ -24,8 +29,10 @@ export const ReviewList = () => {
             <ul>{reviews.map((review) => {
               return (
               <div className='reviewlist'>
+                {loading ? <p>Loading</p> : <button>More Info</button>}
                 <h1 key={review.review_id}>{review.owner}</h1>
                 <p>{review.review_body}</p>
+                {/* <ReviewCard></ReviewCard> */}
               </div>
               )
             })}</ul>
---
+++ northcoders/frontend/fe-nc-games/src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const reviewsUrl = axios.create({
+    baseURL: 'https://my-nc-game-project.onrender.com/api',
+})
+
+export const getReviews = (review_id) => {
+    let path = '/reviews'
+
+    return reviewsUrl
+    .get({path
+    })
+    .then((data) => {
+      return data.reviews
+    })
+}
