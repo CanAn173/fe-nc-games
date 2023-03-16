e24ae34fe0f9fc3f4bcdec02c23fdbc85e4797f2
CanAn173 <108874130+CanAn173@users.noreply.github.com>
2023-03-16 15:50:27 +0000

    Revert "working on task 6"

--- northcoders/frontend/fe-nc-games/src/App.js
+++ northcoders/frontend/fe-nc-games/src/App.js
@@ -1,9 +1,8 @@
 import logo from './Northcoders.jpeg';
 import {Routes, Route} from 'react-router-dom';
-import Header from './component/Header';
-import {ReviewList} from './component/ReviewList';
-import {ReviewCards} from './component/ReviewCard';
-import {CommentCards} from './component/CommentCard';
+import Header from './component/Header'
+import {ReviewList} from './component/ReviewList'
+import {ReviewCards} from './component/ReviewCard'
 import './App.css';
 
 
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path="/reviews" element={<ReviewList />}/>
         <Route path="/reviews/:review_id" element={<ReviewCards />}/>
-        <Route path="/reviews/:review_id/comments" element={<CommentCards />} />
       </Routes>
       </header>
     </div>
